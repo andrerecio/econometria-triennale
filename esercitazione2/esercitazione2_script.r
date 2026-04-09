d857e16a90bee9664f15c504bf78cc602809cbfe
Andrea Recine <148541295+andrerecio@users.noreply.github.com>
2026-04-09 21:56:09 +0200

    New esercitazione2

--- esercitazione2/esercitazione2_script.r
+++ esercitazione2/esercitazione2_script.r
@@ -1,139 +1,179 @@
-#Esercitazione N.2 script
-# Il seguente script riproduce l'esercitazione 2 del corso di Econometria
-# ad eccezione delle tabelle di regressione che sono state create con il pacchetto modelsummary
-# e le tabelle con le statistiche descrittive che sono state create con il pacchetto kableExtra
+# =============================================================================
+# Esercitazione N.2 - Regressione Multipla
+# =============================================================================
 
-# Pacchetti necessari
+# -----------------------------------------------------------------------------
+# Setup: librerie e dati
+# -----------------------------------------------------------------------------
+
 library(tidyverse)
+library(knitr)
 library(modelsummary)
 library(wooldridge)
-
-# Caricamento del dataset
-data("wage1", package = "wooldridge")
-head(wage1)
-
-#Regressione
-# Regressione wage su educ
-# Regressione wage su educ + exper
-# Regressione wage su educ + exper + tenure
 library(fixest)
+
+# Carichiamo il dataset wage1 dal pacchetto wooldridge
+data("wage1", package = "wooldridge")
+
+
+# -----------------------------------------------------------------------------
+# 1. Regressione multipla: educ, exper, tenure
+# -----------------------------------------------------------------------------
+
+# Modello 1: regressione semplice (solo educ)
 reg_wage1 <- feols(wage ~ educ, data = wage1, vcov = "hetero")
+
+# Modello 2: aggiungiamo exper (anni di esperienza nel mercato del lavoro)
 reg_wage2 <- feols(wage ~ educ + exper, data = wage1, vcov = "hetero")
+
+# Modello 3: aggiungiamo tenure (anni con l'attuale datore di lavoro)
 reg_wage3 <- feols(wage ~ educ + exper + tenure, data = wage1, vcov = "hetero")
 
-modelsummary(list("Wage" = reg_wage1, "Wage" = reg_wage2, "Wage" = reg_wage3), output = "markdown", gof_omit = "AIC|BIC|RMSE")
+# Confronto dei tre modelli
+modelsummary(
+  list("Wage" = reg_wage1, "Wage" = reg_wage2, "Wage" = reg_wage3),
+  gof_omit = "AIC|BIC|RMSE"
+)
 
 
-# Regressione con dummy aggiunta
-reg_wage4 <- feols(wage ~ educ + exper + tenure + female, data = wage1, vcov = "hetero")
-modelsummary(list("Wage" = reg_wage3, "Wage" = reg_wage4), output = "markdown", gof_omit = "AIC|BIC|RMSE")
+# -----------------------------------------------------------------------------
+# 2. Correlazione tra educ e exper
+# -----------------------------------------------------------------------------
+
+# La correlazione fra educ e exper è negativa: chi ha più anni di istruzione
+# tende ad entrare nel mercato del lavoro più tardi e quindi ha meno esperienza.
+# Conseguenza: nel modello semplice il coefficiente di educ è distorto verso
+# il basso (sottostima l'effetto dell'istruzione sul salario).
+cor(wage1$educ, wage1$exper)
 
 
-# Cambio unità di misura
+# -----------------------------------------------------------------------------
+# 3. Includiamo una dummy: female
+# -----------------------------------------------------------------------------
 
+# Modello 4: aggiungiamo la dummy female al modello con educ, exper, tenure
+reg_wage4 <- feols(wage ~ educ + exper + tenure + female,
+                   data = wage1, vcov = "hetero")
+
+modelsummary(
+  list("Wage" = reg_wage3, "Wage" = reg_wage4),
+  gof_omit = "AIC|BIC|RMSE"
+)
+
+
+# -----------------------------------------------------------------------------
+# 4. Cambio unità di misura: wage mensile
+# -----------------------------------------------------------------------------
+
+# Salario mensile: assumiamo lavoratori a tempo pieno
+# 7 ore/giorno x 5 giorni/settimana x 4 settimane/mese = 140 ore/mese
 wage1 <- wage1 %>%
-  mutate(wage_monthly = wage*140)
+  mutate(wage_monthly = wage * 140)
+
+# Stimiamo lo stesso modello con wage_monthly come variabile dipendente
+reg_wage_dummy3 <- feols(wage_monthly ~ educ + exper + tenure + female,
+                         data = wage1, vcov = "hetero")
+
+# Confronto: i coefficienti del modello mensile sono i coefficienti orari x 140
+modelsummary(
+  list("Wage" = reg_wage4, "Wage Monthly" = reg_wage_dummy3),
+  gof_omit = "AIC|BIC|RMSE"
+)
 
 
-reg_wage_dummy3 <- feols(wage_monthly ~ educ + exper + tenure + female, data = wage1, vcov = "hetero")
-modelsummary(list("Wage" = reg_wage4, "Wage Monthly" = reg_wage_dummy3), output = "markdown", gof_omit = "AIC|BIC|RMSE") 
+# -----------------------------------------------------------------------------
+# 5. Dummy: wage e genere
+# -----------------------------------------------------------------------------
 
-# Wage e genere:
-
+# Regressione semplice: solo female (come in Esercitazione 1)
 reg_wage_dummy <- feols(wage ~ female, data = wage1, vcov = "hetero")
-reg_wage_dummy2 <- feols(wage ~ female + educ + exper + tenure, data = wage1, vcov = "hetero")
-modelsummary(list("Wage (Esercitazione 1)" = reg_wage_dummy, "Wage" = reg_wage_dummy2), output = "markdown", gof_omit = "AIC|BIC|RMSE")
+
+# Regressione con controlli per educ, exper, tenure
+reg_wage_dummy2 <- feols(wage ~ female + educ + exper + tenure,
+                         data = wage1, vcov = "hetero")
+
+modelsummary(
+  list("Wage (Esercitazione 1)" = reg_wage_dummy, "Wage" = reg_wage_dummy2),
+  gof_omit = "AIC|BIC|RMSE"
+)
 
 
-###########################################################################
-##########################################################################
-#College GPA
+# -----------------------------------------------------------------------------
+# 6. Dummy con categorie multiple: stato civile e genere
+# -----------------------------------------------------------------------------
 
-data("gpa1", package = "wooldridge")
-
-head(gpa1)
-
-# Statistiche descrittive
-statgpa <- gpa1 %>%
-       summarise(
-       colgpa_mean = mean(colGPA, na.rm = TRUE),
-       hsgpa_mean = mean(hsGPA, na.rm = TRUE),
-       act_mean = mean(ACT, na.rm = TRUE),
-  )
-
-statgpa
-
-summary(gpa1$colGPA)
-summary(gpa1$hsGPA)
-
-##Regressione
-reg_gpa1 <- feols(colGPA ~ hsGPA + ACT, data = gpa1, vcov = "hetero")
-modelsummary(list("colGPA" = reg_gpa1), output = "markdown", gof_omit = "AIC|BIC|RMSE")
-
-#Cambio scala
-
-gpa1 <- gpa1 %>%
-  mutate(hsGPA100 = hsGPA *25)
-
-reg_gpa2 <- feols(colGPA ~ hsGPA100 + ACT, data = gpa1, vcov = "hetero")
-modelsummary(list("colGPA100" = reg_gpa2), output = "markdown", gof_omit = "AIC|BIC|RMSE")
-
-
-reg_gpa3 <- feols(colGPA ~ hsGPA + ACT + skipped, data = gpa1, vcov = "hetero")
-modelsummary(list("colGPA" = reg_gpa1, "colGPA" = reg_gpa3), output ="markdown", gof_omit = "AIC|BIC|RMSE")
-
-
-###Effetti PC
-
-reg_gpa_pc1 <- feols(colGPA ~ PC, data = gpa1, vcov = "hetero")
-reg_gpa_pc2 <- feols(colGPA ~ PC + hsGPA + ACT , data = gpa1, vcov = "hetero")
-reg_gpa_pc3 <- feols(colGPA ~ PC + hsGPA + ACT + mothcoll + fathcoll , data = gpa1, vcov = "hetero")
-modelsummary(list("colGPA" = reg_gpa_pc1, "colGPA" = reg_gpa_pc2, "colGPA" = reg_gpa_pc3), output = "markdown", gof_omit = "AIC|BIC|RMSE")
-
-## Dummy con categorie:
-
-
+# Creiamo tre dummy. Il gruppo di riferimento (omesso) sono gli uomini single.
+#  - marrmale:   uomo sposato
+#  - marrfemale: donna sposata
+#  - singfem:    donna single
 wage1 <- wage1 %>%
   mutate(
-    marrmale = ifelse(female == 0 & married == 1, 1, 0),
+    marrmale   = ifelse(female == 0 & married == 1, 1, 0),
     marrfemale = ifelse(female == 1 & married == 1, 1, 0),
-    singfem = ifelse(female == 1 & married == 0, 1, 0))
-
-# Statistiche descrittive
-
-statdummy <- wage1 %>%
-  summarise(
-    marrmale_mean = mean(marrmale, na.rm = TRUE),
-    marrfemale_mean = mean(marrfemale, na.rm = TRUE),
-    singfem_mean = mean(singfem, na.rm = TRUE)
+    singfem    = ifelse(female == 1 & married == 0, 1, 0)
   )
 
+# Le medie delle dummy corrispondono alle proporzioni nel campione
+statdummy <- wage1 %>%
+  summarise(
+    marrmale_mean   = mean(marrmale,   na.rm = TRUE),
+    marrfemale_mean = mean(marrfemale, na.rm = TRUE),
+    singfem_mean    = mean(singfem,    na.rm = TRUE)
+  )
 statdummy
 
+# Regressione con le tre dummy + controlli
+# Il coefficiente di ciascuna dummy va interpretato rispetto al gruppo
+# di riferimento (uomini single).
+reg_wage_sm <- feols(
+  wage ~ marrmale + marrfemale + singfem + educ + exper + tenure,
+  data = wage1, vcov = "hetero"
+)
+
+modelsummary(list("Wage" = reg_wage_sm), gof_omit = "AIC|BIC|RMSE")
 
 
-#Regressione
-reg_wage_sm1 <- feols(wage ~ marrmale + marrfemale + singfem, data = wage1, vcov = "hetero")
-reg_wage_sm2 <- feols(wage ~ marrmale + marrfemale + singfem + educ + exper + tenure, data = wage1, vcov = "hetero")
-modelsummary(list("Wage" = reg_wage_sm1, "Wage" = reg_wage_sm2), output = "markdown", gof_omit = "AIC|BIC|RMSE")
+# -----------------------------------------------------------------------------
+# 7. Trappola delle variabili dummy
+# -----------------------------------------------------------------------------
 
-
-# Collinearità
-wage1 <- wage1 %>% 
+# Creiamo la dummy male = 1 per gli uomini, 0 per le donne
+wage1 <- wage1 %>%
   mutate(male = 1 - female)
 
+# Includendo female e male insieme alle altre dummy si genera collinearità
+# perfetta. R rimuove automaticamente le variabili ridondanti.
+reg_wage_sm_dummy <- feols(
+  wage ~ marrmale + marrfemale + singfem + female + male,
+  data = wage1, vcov = "hetero"
+)
+reg_wage_sm_dummy
 
-reg_wage_sm3 <- feols(wage ~ marrmale + marrfemale + singfem + female + male, data = wage1, vcov = "hetero")
-reg_wage_sm3
 
-## Test ipotesi congiunte
+# -----------------------------------------------------------------------------
+# 8. Test di ipotesi congiunte
+# -----------------------------------------------------------------------------
 
-wald(reg_wage_sm2, keep = "marrmale|marrfemale|singfem")
+# Vogliamo testare H0: i coefficienti di marrmale, marrfemale e singfem
+# sono tutti uguali a zero (cioè non ci sono differenze salariali fra
+# le categorie a parità di educ, exper, tenure).
 
-    
+# Opzione A: funzione wald() del pacchetto fixest
+wald(reg_wage_sm, keep = "marrmale|marrfemale|singfem")
 
+# Opzione B: linearHypothesis() del pacchetto car (la useremo più spesso)
+# install.packages("car")  # se non già installato
 library(car)
-# Test di ipotesi
-linearHypothesis(reg_wage_sm2, c("marrmale = 0", "marrfemale = 0", "singfem = 0"), test = "F")
 
-linearHypothesis(reg_wage_sm2, c("marrmale = 0", "marrfemale = 0", "singfem = 0"))
+# Test F esplicito
+linearHypothesis(
+  reg_wage_sm,
+  c("marrmale = 0", "marrfemale = 0", "singfem = 0"),
+  test = "F"
+)
+
+# Senza specificare test = "F" (di default usa la statistica chi-quadro)
+linearHypothesis(
+  reg_wage_sm,
+  c("marrmale = 0", "marrfemale = 0", "singfem = 0")
+)
